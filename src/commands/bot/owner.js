7e8eb6a6b905bab3c80fcf4c62f86e03f365a73f
adevsolo <reaston.conquest@gmail.com>
2025-03-27 11:28:24 -0400

    Update owner.js

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -7,22 +7,22 @@ module.exports = async (client, interaction, args) => {
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
             name: "👑┆Owner name",
-            value: `Corwin`,
+            value: `Soloo`,
             inline: true,
         },
         {
             name: "🏷┆Discord tag",
-            value: `</Corwin>#0001`,
+            value: `</subtosolo>#0001`,
             inline: true,
         },
         {
             name: "🏢┆Organization",
-            value: `CoreWare`,
+            value: `Solo Central`,
             inline: true,
         },
         {
             name: "🌐┆Website",
-            value: `[https://corwindev.nl](https://corwindev.nl)`,
+            value: `[https://soloo.fun](https://soloo.fun)`,
             inline: true,
         }],
         type: 'editreply'
